c8b96d1bcd71320a963f07c03d2590c9f3ef4789
png2 <pngfou@yahoo.fr>
2015-08-01 20:32:41 +0200

    Add all the filters to the TODO list and upgrade the other list, also add a wtf list with the tags I have no idea what they do

--- modules/Todo.js
+++ modules/Todo.js
@@ -1,7 +1,7 @@
 /**
  * Just to keep track of unimplemented things and make sure the programm doesn't crash when it encounter a valid but not implemented instruction
  */
-module.exports.register = function(commandsProcessor, vocabularyProcessor, uiDispatcher, settings, state) {
+module.exports.register = function({commandsProcessor, vocabularyProcessor, commandFiltersProcessor}) {
     var todoCommands = [
         'ShowBlogImage',
         'LikeBlogImage',
@@ -19,6 +19,10 @@ module.exports.register = function(commandsProcessor, vocabularyProcessor, uiDis
         'EdgeToRuin',
         'EdgeToRuinHold',
         'EdgeToRuinNoHold',
+        'EdgeToRuinHoldSecret',
+        'EdgeToRuinHoldSecret',
+        'EdgeToRuinNoHoldSecret',
+        ,'EdgeToRuinSecret',
         'EdgingDecide',
         'CBT',
         'CBTCock',
@@ -29,7 +33,23 @@ module.exports.register = function(commandsProcessor, vocabularyProcessor, uiDis
         'PlayAvoidTheEdge',
         'ResumeAvoidTheEdge',
         'PlayRedLightGreenLight',
-        'VitalSubAssignment'
+        'PlayVideoTeaseCensorshipSucks',
+        'PlayCensorshipSucks',
+        'PauseAvoidTheEdge',
+        'PauseAvoidTheEdgeNoTaunts',
+        'VitalSubAssignment',
+        'CheckBnB',
+        'CheckJOIVideo',
+        'CheckStrokingState',
+        'CheckTnA',
+        'CheckVideo',
+        'StopTnA',
+        'StrokeFaster',
+        'StrokeFastest',
+        'StrokeSlower',
+        'StrokeSlowest',
+        'StrokeSpeedMax',
+        'InterruptLongEdge'
     ];
 
     todoCommands.forEach((value)=>{
@@ -72,7 +92,10 @@ module.exports.register = function(commandsProcessor, vocabularyProcessor, uiDis
         'TagGarment',
         'TagSexToy',
         'TagTattoo',
-        'TagUnderwear'
+        'TagUnderwear',
+        'SearchImageBlog',
+        'SearchImageBlogAgain',
+        'SendDailyTasks'
     ];
 
     todoVocab.forEach((value)=>{
@@ -80,5 +103,150 @@ module.exports.register = function(commandsProcessor, vocabularyProcessor, uiDis
             return `${value}`;
         });
     });
-    
+
+    var filters = [
+        '10MinuteHold',
+        '15MinuteHold',
+        '1MinuteHold',
+        '2MinuteHold',
+        '30MinuteHold',
+        '3MinuteHold',
+        '45MinuteHold',
+        '4MinuteHold',
+        '5MinuteHold',
+        '60MinuteHold',
+        'AlwaysAllowsOrgasm',
+        'AlwaysRuinsOrgasm',
+        'Angry',
+        'Bratty',
+        'Caring',
+        'CBTLevel1',
+        'CBTLevel2',
+        'CBTLevel3',
+        'CBTLevel4',
+        'CBTLevel5',
+        'ChastityPA',
+        'ChristmasDay',
+        'CockLarge',
+        'CockSizeEnd',
+        'CockSizeLarge',
+        'CockSizeMedium',
+        'CockSizeSmall',
+        'CockSmall',
+        'Crazy',
+        'Cruel',
+        'DommeLevel1',
+        'DommeLevel2',
+        'DommeLevel3',
+        'DommeLevel4',
+        'DommeLevel5',
+        'FirstRound',
+        'HasChastity',
+        'InChastity',
+        'InterruptLongEdge',
+        'LongEdge',
+        'NeverAllowsOrgasm',
+        'NeverRuinsOrgasm',
+        'NewYearsEve',
+        'OftenAllowsOrgasm',
+        'OftenRuinsOrgasm',
+        'OrgasmAllowed',
+        'OrgasmRuined',
+        'RarelyAllowsOrgasm',
+        'RarelyRuinsOrgasm',
+        'RuinTaunt',
+        'SelfOld',
+        'SelfYoung',
+        'ShowBlogImage',
+        'ShowBlowjobImage',
+        'ShowBoobsImage',
+        'ShowButtImage',
+        'ShowCaptionsImage',
+        'ShowCaptionsImage',
+        'ShowDislikedImage',
+        'ShowFemdomImage',
+        'ShowGayImage',
+        'ShowGeneralImage',
+        'ShowGeneralImage',
+        'ShowHardcoreImage',
+        'ShowHentaiImage',
+        'ShowLesbianImage',
+        'ShowLezdomImage',
+        'ShowLikedImage',
+        'ShowLIkedImage',
+        'ShowLocalImage',
+        'ShowMaledomImage',
+        'ShowSoftcoreImage',
+        'ShowTaggedImage',
+        'SometimesAllowsOrgasm',
+        'SometimesruinsOrgasm',
+        'SubBirthday',
+        'SubCircumcised',
+        'SubNotCircumcised',
+        'SubNotPierced',
+        'SubPierced',
+        'Supremacist',
+        'Tag',
+        'TagAss',
+        'TagBoobs',
+        'TagCloseUp',
+        'TagFace',
+        'TagFeet',
+        'TagFullyDressed',
+        'TagHalfDressed',
+        'TagLegs',
+        'TagMasturbating',
+        'TagNaked',
+        'TagNormal',
+        'TagPiercing',
+        'TagPussy',
+        'TagSideView',
+        'TagSucking',
+        'TagTattoo',
+        'TagUnderwear',
+        'ValentinesDay',
+        'VideoBlowjob',
+        'VideoBlowjobDomme',
+        'VideoFemdom',
+        'VideoFemdomDomme',
+        'VideoFemsub',
+        'VideoGeneral',
+        'VideoHardcore',
+        'VideoHardcoreDomme',
+        'VideoLesbian',
+        'VideoLesbianDomme',
+        'VideoSoftcore',
+        'VideoSoftcoreDomme',
+        'VitalSub',
+        'VitalSubAssignment',
+        'Vulgar'
+    ];
+
+    filters.forEach((filter)=>{
+        commandFiltersProcessor.registerFilter(filter,()=>{
+            console.log(`ignore : ${filter}`);
+            return false;
+        });
+    });
+
+    var wtfIsThat = [
+        'CountdownAvoidTheEdge',
+        'Custom1',
+        'Custom2',
+        'EdgingHold',
+        'EdginStop',
+        'EmbedImage',
+        'GiveUpCheck',
+        'InterruptStartStroking',
+        'Module',
+        'NewBlowImage',
+        'SlowDownCheck',
+        'SpeedUpCheck',
+        'TnAFastSlides',
+        'TnASlides',
+        'TnASlowSlides',
+        'VarCheck',
+        'VTLength'
+    ]
+
 };
