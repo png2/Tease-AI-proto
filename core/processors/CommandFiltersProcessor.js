13f959927399124dd8c421c382ff25ad9bfbc1e3
png2 <pngfou@yahoo.fr>
2015-08-01 20:41:41 +0200

    Implementing the command filters processor by using command processor as its base

---
+++ core/processors/CommandFiltersProcessor.js
@@ -0,0 +1,30 @@
+import {CommandsProcessor} from './CommandsProcessor';
+
+/**
+ * Manage the commands and Commands filters used in all the list files : taunts, cbt, edging, etc.
+ */
+export class CommandFiltersProcessor extends CommandsProcessor{
+
+    constructor(ui,settings, state) {
+        super(ui,settings,state);
+    }
+
+    setListParser(listParser) {
+        super.setScriptParser(listParser);
+    }
+
+    registerFilter(filterName,filter) {
+        super.registerCommand(filterName,(parser, ui, settings, state, params)=>{
+            if(!filter(settings, state, params)) {
+                parser.ignoreLine();
+            }
+        });
+
+        super.registerCommand(`Not${filterName}`,(parser, ui, settings, state, params)=>{
+            if(filter(settings, state, params)) {
+                parser.ignoreLine();
+            }
+        });
+    }
+
+}
